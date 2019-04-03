6cc5050435c6b8144bacc9b97461dfd623bfb5fd
DESKTOP-UVTGU5F\climonggi <limonggi@gmail.com>
2019-04-04 00:33:49 +0200

    ROUNDING UP
    (info related to previous commit)
    
    First styles for stores.
    All modules working.
    Open stores first.
    Fixed bugs from storeAvailability.
    
    Still work to do, search by tags not done yet.
    
    All good.

---
+++ webpack.client.config.js
@@ -0,0 +1,73 @@
+// Work around for https://github.com/angular/angular-cli/issues/7200
+
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { AngularCompilerPlugin } = require('@ngtools/webpack');
+
+module.exports = {
+  entry: {
+    webworker: [
+      "./src/workerLoader.ts"
+    ],
+    main: [
+      "./src/main.ts"
+    ],
+    polyfills: [
+      "./src/polyfills.ts"
+    ]
+  },
+  target: 'node',
+  resolve: { extensions: ['.ts', '.js'] },
+  optimization: {
+    splitChunks: {
+      name: "inline"
+    }
+  },
+  output: {
+    // Puts the output at the root of the dist folder
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: 'ts-loader' },
+      {
+        // Mark files inside `@angular/core` as using SystemJS style dynamic imports.
+        // Removing this will cause deprecation warnings to appear.
+        test: /(\\|\/)@angular(\\|\/)core(\\|\/).+\.js$/,
+        parser: { system: true },
+      },
+    ]
+  },
+  plugins: [
+    new webpack.ContextReplacementPlugin(
+      // fixes WARNING Critical dependency: the request of a dependency is an expression
+      /(.+)?angular(\\|\/)core(.+)?/,
+      path.join(__dirname, 'src'), // location of your src
+      {} // a map of your routes
+    ),
+    new webpack.ContextReplacementPlugin(
+      // fixes WARNING Critical dependency: the request of a dependency is an expression
+      /(.+)?express(\\|\/)(.+)?/,
+      path.join(__dirname, 'src'),
+      {}
+    ),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      excludeChunks: [
+        "webworker"
+      ],
+      chunksSortMode: "none"
+    }),
+    new AngularCompilerPlugin({
+      mainPath: "./src/main.ts",
+      entryModule: './src/app/app.module#AppModule',
+      tsConfigPath: "src/tsconfig.app.json",
+      exclude: ['./server.ts'],
+      sourceMap: true,
+      platform: 0
+    }),
+  ]
+};
